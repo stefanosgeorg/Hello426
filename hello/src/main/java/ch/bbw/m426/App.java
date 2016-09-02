cd91d17a2663a7d037e7282b668b292949bc12bb
5ia14stgeorgalis <5ia14stgeorgalis@SYSW2122.gibwintra.ch>
2016-09-02 09:38:00 +0200

    first commit of the source

---
+++ hello/src/main/java/ch/bbw/m426/App.java
@@ -0,0 +1,13 @@
+package ch.bbw.m426;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
